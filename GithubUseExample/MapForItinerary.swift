cd304c99553c25e39aa2d0046ae8b5339a7e2b9c
Christian Boes <cboes2402@gmail.com>
2018-03-25 18:21:06 +0200

    New map

---
+++ GithubUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GithubUseExample
+//
+//  Created by Christian Boes on 25/03/2018.
+//  Copyright © 2018 Christian Boes. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
